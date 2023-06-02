5cb752d459d799526abbf730f0b4fef9ac2979bf
Haritha982 <114345177+Haritha982@users.noreply.github.com>
2023-06-02 14:57:39 +0530

    created repo
    repo created

---
+++ nss_connect/android/app/src/main/kotlin/com/example/nss_connect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nss_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ nss_connect/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Login(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+ 
+
+
+
+  
