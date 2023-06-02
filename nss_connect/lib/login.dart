bafc2859372e7b31b555d719a322903a854f892f
Haritha982 <114345177+Haritha982@users.noreply.github.com>
2023-06-02 22:54:21 +0530

    username,password
    username and password box added

--- nss_connect/lib/login.dart
+++ nss_connect/lib/login.dart
@@ -5,8 +5,110 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(children: [
+      Container(
+        
+        padding: const EdgeInsets.only(top: 50,right: 10),
+        height: MediaQuery.of(context).size.height*0.25,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Dropdown',
+            ),
+          ],
+        ),
+        ),
+        
+        Container(
+          padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height*0.6,
+          width: MediaQuery.of(context).size.width,
+          child: Card(
+            color: Colors.white,
+            elevation: 4,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+                SizedBox(
+                    height: MediaQuery.of(context).size.width*0.06,
+                  ),
+                  Container(
+                    alignment: AlignmentDirectional.topStart,
+                    padding: EdgeInsets.only(bottom: 10, left: 30),
+                    child: Text('Your Username',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                    ),),
+                  ),
+                 Container(
+                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                       border: Border.all(
+                       color: Colors.grey.withOpacity(0.3), // Change the border color here
+                      ),
+                     ),
+                      width: MediaQuery.of(context).size.width*0.8,
+                      child: TextField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 10),
+                          hintText: 'Enter your username',
+                          hintStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                    ),
+                  Container(
+                    alignment: AlignmentDirectional.topStart,
+                    padding: EdgeInsets.only(bottom: 10, left: 30,top: 10),
+                    child: Text('Password',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                    ),),
+                  ),
+                  Container(
+                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                       border: Border.all(
+                       color: Colors.grey.withOpacity(0.3), // Change the border color here
+                      ),
+                     ),
+                      width: MediaQuery.of(context).size.width*0.8,
+                      child: TextField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 10),
+                          hintText: 'Enter your password',
+                          hintStyle: TextStyle(
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                      ),
+                    ),
+
+            ]),
+          ),
+        ),
+    ]),
     );
   }
 }
