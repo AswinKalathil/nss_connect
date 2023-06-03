1a717035de0b23d3d3d29185f22134b5c153dc95
Thoukheer <thoukheeribn@gmail.com>
2023-06-03 23:06:38 +0530

    register crete

---
+++ nss_connect/lib/register.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:nss_connect/regcard.dart';
+import 'credCard.dart';
+import 'Secretary.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/doodleBack.png'),
+              fit: BoxFit.cover,
+            )),
+        child: Column(children: [
+          Container(
+            padding: const EdgeInsets.only(top: 50, right: 10),
+            height: MediaQuery.of(context).size.height * 0.25,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+              ],
+            ),
+          ),
+            RegCard()
+        ]),
+      ),
+    );
+  }
+}
