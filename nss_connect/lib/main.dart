cbe70e10ffbaaffa98746aa54b2bed8630b7715c
AswinKalathil <97769498+AswinKalathil@users.noreply.github.com>
2023-06-09 01:22:15 +0530

    splash screen added
    upadat pubspec.yamel

--- nss_connect/lib/main.dart
+++ nss_connect/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:nss_connect/register.dart';
 import 'login.dart';
@@ -15,19 +17,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: primaryViolet,
-        fontFamily: 'Nexa',
-      ),
-      initialRoute: Home.id,
-      routes: {
-        Home.id: (ctx) => Home(),
-        Login.id: (ctx) => Login(),
-        Register.id: (ctx) => Register(),
-        ConfirmData.id: (ctx) => ConfirmData(),
-      },
-      home: const Login(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: primaryViolet,
+          fontFamily: 'Nexa',
+        ),
+        // initialRoute: Home.id,
+        routes: {
+          Home.id: (ctx) => Home(),
+          Login.id: (ctx) => Login(),
+          Register.id: (ctx) => Register(),
+          ConfirmData.id: (ctx) => ConfirmData(),
+        },
+        home: AnimatedSplashScreen(
+            duration: 1500,
+            splash: Icons.home,
+            nextScreen: Home(),
+            splashTransition: SplashTransition.rotationTransition,
+            pageTransitionType: PageTransitionType.rightToLeft,
+            centered: true,
+            backgroundColor: Color.fromARGB(255, 237, 83, 83))
+        // home: const Login(),
+        );
   }
 }
