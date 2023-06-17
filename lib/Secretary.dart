cc064a5ccf11c3378dd8aa293220dffd971bc16a
Thoukheer <thoukheeribn@gmail.com>
2023-06-17 08:36:29 +0530

    secretary page

--- lib/Secretary.dart
+++ lib/Secretary.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
 
-class SecretaryDashboard extends StatelessWidget {
-  const SecretaryDashboard({super.key});
-  static const String id = 'secretaryDashboard';
+
+class SecretaryDashboard extends StatefulWidget{
+ const SecretaryDashboard({super.key});
+ static const String id = 'secretaryDashboard';
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('secretary Dashboard'),
-      ),
-      body: Center(
-        child: Text('This is the destination page.'),
-      ),
-    );
-  }
+  State<SecretaryDashboard> createState() => _SecretaryDashboardState();
+
 }
+
+class _SecretaryDashboardState extends State<SecretaryDashboard>{
+  int myCurrentIndex=3;
+  @override
+
+
+  Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('secretary Dashboard'),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      selectedItemColor: Colors.purple,
+      unselectedItemColor: Colors.blueGrey,
+      currentIndex: myCurrentIndex,
+      iconSize: 30,
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label:'Home'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.monitor),
+            label:'Monitor'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.event_note_outlined),
+            label:'Event'
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.miscellaneous_services_outlined),
+            label:'Services'
+        ),
+      ],),
+  );
+}
+
+}
+
+
+
+
