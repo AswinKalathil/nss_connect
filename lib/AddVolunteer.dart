ca2611f229d8228532daee0e573aae2bb78a1b5e
Thoukheer <thoukheeribn@gmail.com>
2023-06-29 19:09:55 +0530

    Update AddVolunteer.dart

--- lib/AddVolunteer.dart
+++ lib/AddVolunteer.dart
@@ -1,20 +1,311 @@
 import 'package:flutter/material.dart';
-import 'package:nss_connect/colors.dart';
+import 'package:nss_connect/widgetStyles.dart';
 
-class AddVolunteer extends StatelessWidget {
+
+class AddVolunteer extends StatefulWidget {
   const AddVolunteer({super.key});
   static const String id = 'AddVolunteer';
+
+  @override
+  State<AddVolunteer> createState() => _AddVolunteerState();
+}
+
+class _AddVolunteerState extends State<AddVolunteer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          'Add New Volunteers',
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: secondaryColor),
+    return GestureDetector(
+      onTap: ()=>
+          FocusScope.of(context).requestFocus(FocusNode())
+      ,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/doodleBack.png'),
+                fit: BoxFit.cover,
+              )),
+          child: Column(children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .15,
+            ),
+            DataCard()
+          ]),
         ),
       ),
     );
   }
 }
+
+
+
+class DataCard extends StatefulWidget {
+  const DataCard({super.key});
+
+  @override
+  State<DataCard> createState() => _DataCardState();
+}
+
+class _DataCardState extends State<DataCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      // height: MediaQuery.of(context).size.height * 0.65,
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+        shape: CardShape(padding: 50),
+        color: Colors.white,
+        elevation: 10,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  'Register Secretary',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Nexa',
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: AlignmentDirectional.topStart,
+                padding: EdgeInsets.only(bottom: 10, left: 30),
+                child: Text(
+                  'Username',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(
+                    color: Colors.grey
+                        .withOpacity(0.3), // Change the border color here
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  style: TextStyle(),
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                    hintText: 'Enter username',
+                    hintStyle: TextStyle(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.topStart,
+                padding: EdgeInsets.only(bottom: 10, left: 30),
+                child: Text(
+                  'Email',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(
+                    color: Colors.grey
+                        .withOpacity(0.3), // Change the border color here
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  style: TextStyle(),
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                    hintText: 'Enter EmailId ',
+                    hintStyle: TextStyle(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                ),
+              ), Container(
+                alignment: AlignmentDirectional.topStart,
+                padding: EdgeInsets.only(bottom: 10, left: 30),
+                child: Text(
+                  'Phone Number',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(
+                    color: Colors.grey
+                        .withOpacity(0.3), // Change the border color here
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  style: TextStyle(),
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                    hintText: 'Enter Phone Number',
+                    hintStyle: TextStyle(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: AlignmentDirectional.topStart,
+                padding: EdgeInsets.only(bottom: 10, left: 30, top: 10),
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade600,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(
+                    color: Colors.grey
+                        .withOpacity(.3), // Change the border color here
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  style: TextStyle(),
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10),
+                    hintText: 'Enter password',
+                    hintStyle: TextStyle(
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                  ),
+                ),
+              ),
+
+
+              SizedBox(
+                height: 15,
+              ),
+
+              //forgot password container
+
+              SizedBox(
+                height: 20,
+              ),
+
+              LongButton(buttonText: 'Register', buttonAction: () {})
+            ]),
+      ),
+    );
+  }
+}
+
+//
+//
+// class VolunteerCard extends StatefulWidget {
+//   static const String id = 'VolunteerCard';
+//   @override
+//   _VolunteerCardState createState() => _VolunteerCardState();
+// }
+//
+// class _VolunteerCardState extends State<VolunteerCard> {
+//   TextEditingController _usernameController = TextEditingController();
+//   TextEditingController _emailController = TextEditingController();
+//   TextEditingController _phoneController = TextEditingController();
+//   TextEditingController _passwordController = TextEditingController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             TextField(
+//               controller: _usernameController,
+//               decoration: InputDecoration(
+//                 labelText: 'Username',
+//               ),
+//             ),
+//             TextField(
+//               controller: _emailController,
+//               decoration: InputDecoration(
+//                 labelText: 'Email',
+//               ),
+//             ),
+//             TextField(
+//               controller: _phoneController,
+//               decoration: InputDecoration(
+//                 labelText: 'Phone Number',
+//               ),
+//             ),
+//             TextField(
+//               controller: _passwordController,
+//               decoration: InputDecoration(
+//                 labelText: 'Password',
+//               ),
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 // Perform registration logic here
+//                 String username = _usernameController.text;
+//                 String email = _emailController.text;
+//                 String phone = _phoneController.text;
+//                 String password = _passwordController.text;
+//
+//                 // Validate and process the registration details
+//                 // ...
+//               },
+//               child: Text('Register'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
