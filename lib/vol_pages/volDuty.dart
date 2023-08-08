ca307e1c157ff7c45b1b9dd4be981a4f9e7bd018
Thoukheer <thoukheeribn@gmail.com>
2023-08-08 22:05:21 +0530

    volunteer duty

--- lib/vol_pages/volDuty.dart
+++ lib/vol_pages/volDuty.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:nss_connect/colors.dart';
 
+import '../models/dutymodel.dart';
+import '../widgetStyles.dart';
+
 class VolDuty extends StatefulWidget {
   const VolDuty({super.key});
   static const String id = 'certificaterequest';
@@ -11,6 +14,93 @@ class VolDuty extends StatefulWidget {
 class _VolDutyState extends State<VolDuty> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width100 = MediaQuery.of(context).size.width;
+    double height100 = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
+    return ListView(
+      children: dutyList.map((dutyObject) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: width100 * .9,
+              height: height100 * 0.18, //height adjust
+              child: Card(
+                elevation: 5,
+                shape: CardShape(),
+                child: Row(
+                  children: [
+                    // Container(
+                    //   width: width100 * 0.3,
+                    //   decoration: BoxDecoration(
+                    //     image: DecorationImage(
+                    //       fit: BoxFit.fill,
+                    //       image: AssetImage(dutyObject.eventImagepath),
+                    //     ),
+                    //     borderRadius: BorderRadius.only(
+                    //         topLeft: Radius.circular(5),
+                    //         bottomLeft: Radius.circular(5)),
+                    //   ),
+                    // ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(5),
+                              bottomRight: Radius.circular(5)),
+                        ),
+                        height: height100 *
+                            0.25, //========================height  adjest
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${dutyObject.dutyTitle}',
+                              style: TextStyle(
+                                  fontFamily: 'Source Code Pro',
+                                  fontSize: width100 * 0.07,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${dutyObject.dutyDescription}',
+                              style: TextStyle(
+                                  fontFamily: 'Source Code Pro',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: width100 * 0.0375),
+                            ),
+                            Text(
+                              '${dutyObject.dutyDate}',
+                              style: TextStyle(
+                                  fontFamily: 'Source Code Pro',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: width100 * 0.0375),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    ButtonBar(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+
+                            // Handle button press
+
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
   }
 }
