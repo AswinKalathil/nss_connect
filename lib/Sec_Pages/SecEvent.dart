251ea024279fe2c560fd22490a182c65a8c9cfd7
AswinKalathil <97769498+AswinKalathil@users.noreply.github.com>
2023-08-08 15:02:28 +0530

    event need fix

--- lib/Sec_Pages/SecEvent.dart
+++ lib/Sec_Pages/SecEvent.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nss_connect/colors.dart';
+import 'package:nss_connect/models/events.dart';
 
+import '../widgetStyles.dart';
 import 'EventApp.dart';
 
 class SecEvent extends StatelessWidget {
@@ -8,16 +10,100 @@ class SecEvent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width100 = MediaQuery.of(context).size.width;
+    double height100 = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
     return Scaffold(
-
+      body: secEventBody(
+        height100: height100,
+        width100: width100,
+      ),
       floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // Add your action here
-        Navigator.pushNamed(context,EventApp.id);
-      },
-      child: Icon(Icons.add),
-      backgroundColor: primaryButton, // Customize FAB color
-    ),
+        onPressed: () {
+          // Add your action here
+          Navigator.pushNamed(context, EventApp.id);
+        },
+        child: Icon(Icons.add),
+        backgroundColor: primaryButton, // Customize FAB color
+      ),
+    );
+  }
+}
+
+class secEventBody extends StatelessWidget {
+  const secEventBody({
+    super.key,
+    required this.width100,
+    required this.height100,
+  });
+  final double width100;
+  final double height100;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: eventList.map((eventObject) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: width100 * .9,
+              child: Card(
+                elevation: 5,
+                shape: CardShape(),
+                child: Row(
+                  children: [
+                    Container(
+                      width: width100 * 0.3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(eventObject.eventImagepath),
+                        ),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(5),
+                            bottomLeft: Radius.circular(5)),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(5),
+                              bottomRight: Radius.circular(5)),
+                        ),
+                        height: height100 *
+                            0.25, //========================height  adjest
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${eventObject.eventTitle}',
+                              style: TextStyle(
+                                  fontFamily: 'Source Code Pro',
+                                  fontSize: width100 * 0.07,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${eventObject.eventDescription}',
+                              style: TextStyle(
+                                  fontFamily: 'Source Code Pro',
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: width100 * 0.0375),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }).toList().,
     );
   }
 }
--- lib/models/events.dart
+++ lib/models/events.dart
@@ -1,18 +1,38 @@
+import 'package:intl/intl.dart';
+
+
 class eventItem {
   final String eventTitle;
   final String eventImagepath;
   final String eventDescription;
+  late DateTime eventDate;
 
-  const eventItem({
+ eventItem({
     required this.eventDescription,
     required this.eventImagepath,
     required this.eventTitle,
+    required this.eventDate, 
   });
 }
 
 List<eventItem> eventList = [
-  eventItem(eventDescription: 'State Annual NSS Meet', eventImagepath: 'assets/images/1.png', eventTitle: 'Annual Meet'),
-  eventItem(eventDescription: 'The upcycling event', eventImagepath: 'assets/images/2.png', eventTitle: 'Bhumi',),
-  eventItem(eventDescription: 'First year orientation', eventImagepath: 'assets/images/3.png', eventTitle: 'Orientation',),
-  eventItem(eventDescription: 'Blood Donation Camp', eventImagepath: 'assets/images/4.png', eventTitle: 'Dakshatha'),
+  eventItem(
+      eventDescription: 'State Annual NSS Meet',
+      eventImagepath: 'assets/images/1.png',
+      eventTitle: 'Annual Meet'),
+      
+  eventItem(
+    eventDescription: 'The upcycling event',
+    eventImagepath: 'assets/images/2.png',
+    eventTitle: 'Bhumi',
+  ),
+  eventItem(
+    eventDescription: 'First year orientation',
+    eventImagepath: 'assets/images/3.png',
+    eventTitle: 'Orientation',
+  ),
+  eventItem(
+      eventDescription: 'Blood Donation Camp',
+      eventImagepath: 'assets/images/4.png',
+      eventTitle: 'Dakshatha'),
 ];
